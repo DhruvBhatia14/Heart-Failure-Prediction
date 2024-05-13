127202ca245239a797a5c4c42a15bfde4c269098
ajeet36 <102666772+ajeet36@users.noreply.github.com>
2024-05-13 16:02:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def run():
     def load_model(model_path):
         return pd.read_pickle(model_path)
 
-    model = pd.read_pickle("xgboost_hear_disease_detection_v1.pkl")
+    model = pd.read_pickle("gboost_heart_disease_detection_v1.pkl")
 
     st.markdown(
         """
